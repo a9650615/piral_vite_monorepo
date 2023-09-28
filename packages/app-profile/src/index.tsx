35474ad7934ba554578b3a9703a7cbb47637061b
Ming You Tsai <mingyou.tsai@onedegree.hk>
2023-09-28 17:36:44 +0800

    feat: add route for test page

--- packages/app-profile/src/index.tsx
+++ packages/app-profile/src/index.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import type { PiletApi } from "app-shell";
+import type { PiletApi } from "@package/app-shell";
 
 const Page = React.lazy(() => import("./Page"));
 
 export function setup(app: PiletApi) {
   app.registerPage("/page", Page);
   app.showNotification("Hello from Piral!");
-  app.registerMenu(() => (
-    <a href='https://docs.piral.io' target='_blank'>
-      Documentation
-    </a>
-  ));
+  app.registerMenu(() => <Link to='/page'>Documentation</Link>);
   app.registerTile(() => <div>Welcome to Piral!</div>, {
     initialColumns: 2,
     initialRows: 1,
