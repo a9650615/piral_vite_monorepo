78456f82198abdc551649c3dc4756b4882a295f0
Ming You Tsai <mingyou.tsai@onedegree.hk>
2023-10-06 16:10:49 +0800

    feat: sjow profile content in profile page

--- packages/app-profile/src/index.tsx
+++ packages/app-profile/src/index.tsx
@@ -7,7 +7,7 @@ const Page = React.lazy(() => import("./Page"));
 export function setup(app: PiletApi) {
   app.registerPage("/page", Page);
   app.showNotification("Hello from Piral!");
-  app.registerMenu(() => <Link to='/page'>Documentation</Link>);
+  app.registerMenu(() => <Link to='/page'>Profile</Link>);
   app.registerTile(() => <div>Welcome to Piral!</div>, {
     initialColumns: 2,
     initialRows: 1,
